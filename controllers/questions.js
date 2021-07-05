84aca488ce9e25430bb34614c57ae9dc362f3987
Boubacar DIALLO <bdiallo@solocal.com>
2021-07-05 11:09:56 +0200

    modification controllers questions

--- controllers/questions.js
+++ controllers/questions.js
@@ -33,25 +33,24 @@ var Request = require('tedious').Request;
 
 // method for get entries in Questions (GET)
 exports.GetQuestions =function(req, res) {
-    var q = "Select * from Questions "
-    var check = true;
+    var q = "SELECT Q.QuestionId, T.Name, L.Name, D.Name, S.Name, Q.Text, Q.Passed, Q.Auxiliar from Questions Q, Languages L, Types T, Difficulties D, Subjects S where Q.QType=T.TypeId AND Q.QLanguage=L.LanguageId" +
+        " AND Q.QDifficulty=D.DifficultyId AND Q.QSubject=S.SubjectId";
+    const cmdType = "(SELECT TypeId from Types Where Name='" + req.query.Type + "'),";
+    const cmdDifficulty = "(SELECT DifficultyId from Difficulties Where Name='" + req.query.Difficulty + "')";
+    const cmdLanguage = "(SELECT LanguageId from Languages Where Name='" + req.query.Language + "')";
+    const cmdSubject = "(SELECT SubjectId from Subject Where Name='" + req.query.Subject + "' AND WHERE LanguageId="
+        + "(SELECT LanguageId from Languages Where Name='" + req.query.Language + "'))";
     if(req.query.Type) {
-        check = false;
-        q += "WHERE QType = '" + req.query.Type + "' ";
+        q += " AND QType =" + cmdType;
     }
     if(req.query.Language) {
-        q += check ? "WHERE " : "AND ";
-        q += "QLanguage = '" + req.query.Language + "' ";
-        check = false;
+        q += " AND QLanguage =" + cmdLanguage;
     }
     if(req.query.Subjects) {
-        q += check ? "WHERE " : "AND ";
-        q += "QSubject = '" + req.query.Subjects + "' ";
-        check = false;
+        q += " AND QSubject =" + cmdSubject;
     }
     if(req.query.Difficulty) {
-        q += check ? "WHERE " : "AND ";
-        q += "QDifficulty = '" + req.query.Difficulty + "' ";
+        q += " AND QDifficulty =" + cmdDifficulty;
     }
     console.log(q);
     request = new Request(q, function(err) {
@@ -62,20 +61,17 @@ exports.GetQuestions =function(req, res) {
     });
 
     var result = [];
-    request.on('row', function(columns) {
+    request.on('row', async function(columns) {
         var QuestionId = columns[0].value;
+        var Text = columns[5].value;
+        var Auxiliar = columns[6].value;
+        var Passed = columns[7].value;
         var QType = columns[1].value;
         var QLanguage = columns[2].value;
         var QDifficulty = columns[3].value;
         var QSubject = columns[4].value;
-        var Text = columns[5].value;
-        var Auxiliar = columns[6].value;
-        var Passed = columns[7].value;
-
         result.push({ QuestionId, QType, QLanguage, QDifficulty, QSubject, Text, Auxiliar, Passed});
-
     });
-
     request.on('requestCompleted', function () {
         return res.status(200).send(result);
     });
@@ -86,8 +82,13 @@ exports.GetQuestions =function(req, res) {
 // method for create entry in Questions (POST)
 exports.CreateQuestion = function (req, res) {
     const cmdValue  = "INSERT INTO Questions (QType, QLanguage, QDifficulty, QSubject, Text, Passed, Auxiliar) VALUES (" +
-        req.query.Type + ',' + req.query.Language + ',' + req.query.Difficulty + ',' + req.query.Subject + ', \'' + req.query.Text + '\', 0,' + req.query.Auxiliar + ')';
-    request = new Request(cmdValue, function(err) {
+        "(SELECT TypeId FROM Types WHERE Name='" + req.query.Type + "')," +
+        "(SELECT DifficultyId FROM Difficulties WHERE Name='" + req.query.Difficulty + "')," +
+        "(SELECT LanguageId FROM Languages WHERE Name='" + req.query.Language + "')," +
+        "(SELECT SubjectId FROM Subjects WHERE Name='" + req.query.Subject + "' AND LanguageId=(SELECT LanguageId FROM Languages WHERE Name='"+ req.query.Language + "'))" +
+        ', \'' + req.query.Text + '\', 0,' + req.query.Auxiliar + ')';
+
+    let request = new Request(cmdValue, function(err) {
         if (err) {
             console.log(err);
             return res.status(500).send(err.message);
@@ -103,23 +104,30 @@ exports.CreateQuestion = function (req, res) {
 // method for update entry in Questions (PUT)
 exports.UpdateQuestion = async function (req, res) {
 
-    const cmdRequest = "SELECT * from Questions Where QuestionId=" + req.query.QuestionId;
-    requestSelect = new Request(cmdRequest, function(err) {
+    const cmdRequest = "SELECT Q.QuestionId, T.Name, L.Name, D.Name, S.Name, Q.Text, Q.Passed, Q.Auxiliar from Questions Q, Languages L, Types T, Difficulties D, Subjects S where Q.QType=T.TypeId AND Q.QLanguage=L.LanguageId" +
+        "AND Q.QDifficulty=D.DifficultyId AND Q.QSubject=S.SubjectId AND QuestionId=" + req.query.QuestionId;
+    let requestSelect = new Request(cmdRequest, function(err) {
         if (err) {
             console.log(err);
         }
     });
 
     requestSelect.on('row', function(columns) {
+        var Text = columns[5].value;
         var QType = columns[1].value;
         var QLanguage = columns[2].value;
-        var Text = columns[5].value;
-        const cmdValue = "UPDATE Questions SET "
-            + 'QType = ' + req.query.Type + ' , QLanguage = ' + req.query.Language + ' , QDifficulty = '
-            + req.query.Difficulty + ' , QSubject = ' + req.query.Subject + ' , Text = \'' + req.query.Text +
-            '\' , Passed = '+ req.query.Passed + ' , Auxiliar = ' + req.query.Auxiliar + ' WHERE QType=' + QType +
-            " AND QLanguage=" + QLanguage + " AND Text ='" + Text + "'";
-        request = new Request(cmdValue, function(err) {
+        var QDifficulty = columns[3].value;
+        const cmdValue = "UPDATE Questions SET " +
+            "QType= (SELECT TypeId FROM Types WHERE Name='" + req.query.Type + "')," +
+            "QDifficulty= (SELECT DifficultyId FROM Difficulties WHERE Name='" + req.query.Difficulty + "')," +
+            "QLanguage=(SELECT LanguageId FROM Languages WHERE Name='" + req.query.Language + "')," +
+            "QSubject= (SELECT SubjectId FROM Subjects WHERE Name='" + req.query.Subject + "' AND LanguageId=(SELECT LanguageId FROM Languages WHERE Name='"+ req.query.Language + "'))" +
+            ', Text= \'' + req.query.Text + '\', Passed =' + req.query.Passed + ', Auxiliar=' + req.query.Auxiliar
+             + " WHERE QType=(SELECT TypeId FROM Types WHERE Name='" + QType + "')," +
+            " AND QLanguage=(SELECT LanguageId FROM Languages WHERE Name='" + QLanguage + "')," +
+            " AND QSubject=(SELECT DifficultyId FROM Difficulties WHERE Name='" + QDifficulty + "')," +
+            "  AND Text ='" + Text + "'";
+        let request = new Request(cmdValue, function(err) {
             if (err) {
                 console.log(err);
                 return res.status(500).send(err.message);
@@ -151,20 +159,25 @@ exports.UpdateQuestion = async function (req, res) {
 
 // method for delete entry in Questions (DELETE)
 exports.DeleteQuestion = function (req, res) {
-    const cmdRequest = "SELECT * from Questions Where QuestionId=" + req.query.QuestionId;
-    requestSelect = new Request(cmdRequest, function(err) {
+    const cmdRequest = "SELECT Q.QuestionId, T.Name, L.Name, D.Name, S.Name, Q.Text, Q.Passed, Q.Auxiliar from Questions Q, Languages L, Types T, Difficulties D, Subjects S where Q.QType=T.TypeId AND Q.QLanguage=L.LanguageId" +
+        "AND Q.QDifficulty=D.DifficultyId AND Q.QSubject=S.SubjectId AND QuestionId=" + req.query.QuestionId;
+    let requestSelect = new Request(cmdRequest, function(err) {
         if (err) {
             console.log(err);
         }
     });
 
     requestSelect.on('row', function(columns) {
+        var Text = columns[5].value;
         var QType = columns[1].value;
         var QLanguage = columns[2].value;
-        var Text = columns[5].value;
-        const cmdValue = "DELETE FROM Questions WHERE QType=" + QType  + " AND QLanguage=" + QLanguage + " AND Text ='" + Text + "'";
-
-        request = new Request(cmdValue, function(err) {
+        var QDifficulty = columns[3].value;
+        const cmdValue = "DELETE FROM Questions WHERE" +
+            " QType=(SELECT TypeId FROM Types WHERE Name='" + QType + "')," +
+            " AND QLanguage=(SELECT LanguageId FROM Languages WHERE Name='" + QLanguage + "')," +
+            " AND QSubject=(SELECT DifficultyId FROM Difficulties WHERE Name='" + QDifficulty + "')," +
+            "  AND Text ='" + Text + "'";
+        let request = new Request(cmdValue, function(err) {
             if (err) {
                 console.log(err);
                 return res.status(500).send(err.message);
@@ -193,3 +206,4 @@ exports.DeleteQuestion = function (req, res) {
     }
 
 }
+
